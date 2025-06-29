f77ec40a253562bf8ed9692acfeca19185ff97d0
Daniel Olaoluwa <danielolaoluwa@gmail.com>
2025-06-29 02:45:53 +0100

    contactus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
-import WhyChoose from "./Components/Section/WhyChoose/WhyChoose";
+// import WhyChoose from "./Components/Section/WhyChoose/WhyChoose";
+import Contactus  from "./Components/Section/ContactUs/ContactUs"
+
 // import { BrowserRouter } from "react-router-dom";
 // import AppRoutes from "./AppRoutes/AppRoutes";
 
 function App() {
   return (
-    <WhyChoose />
+    // <WhyChoose />
+    <Contactus />
     // <BrowserRouter>
     //   <AppRoutes />
     // </BrowserRouter>
--- src/Components/Section/ContactUs/ContactUs.jsx
+++ src/Components/Section/ContactUs/ContactUs.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import styles from './ContactUs.module.css'
+import './Contact.css';
+import image from '../../../Assets/Img/ContactUs.svg'
+
 
 const ContactUs = () => {
   return (
-    <div className={styles.contactUs}>ContactUs</div>
+    <div className="Contact-us-container">
+      <div className="form-container">
+        <div className="contact-header">
+          <p className='header-title'>Contact Us</p>
+          <p className='header-details'>if you have any query please contact us </p>
+        </div>
+        <div className="main-form">
+          <label htmlFor="name">Name*</label><br />
+          <input type="text" name="Name" id="name" />
+          <br />
+          <label htmlFor="email">Email Address*</label><br />
+          <input type="email" name="email" id="email" />
+          <br />
+          <label htmlFor="subject">Subject</label><br />
+          <input type="text" name="subject" id="subject" />
+          <br />
+          <label htmlFor="message">Message*</label><br />
+          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+          <br />
+          <input className='submit' type="submit" value="Send Message" id="submit"/>
+          
+        </div>
+      </div>
+      <div className="image-container">
+        <img src={image} className='image' alt="contact" />
+      </div>
+    </div>
   )
 }
 
