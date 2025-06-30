6720bac747f4215080e4e9edee247e086e6d74df
Fijabs <161024459+Fijabs@users.noreply.github.com>
2025-06-30 09:06:17 +0100

    Started working

--- src/Components/Section/FrequentlyAskedQty/FrequentlyAskedQty.jsx
+++ src/Components/Section/FrequentlyAskedQty/FrequentlyAskedQty.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import styles from './FrequentlyAskedQty.module.css'
+import FAQimage from './Assets/Img/FAQ.svg'
 
 const FrequentlyAskedQty = () => {
   return (
-    <div className={styles.frequentlyAskedQty}>FrequentlyAskedQty</div>
+    <div className={styles.frequentlyAskedQty}>
+      <img src="./src/AssetsImg/FAQ.svg" alt="FAQ image" />
+      <div className='body-text'>
+        <span className='Intro'><b>Frequently Asked Questions</b></span>
+        <br></br>
+        <b>Where can I watch?</b>
+        <br></br>
+       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus hic quisquam debitis nobis omnis recusandae at earum nemo natus, praesentium dolorem veniam, eligendi dolorum sapiente vel eos reprehenderit, tenetur nam?
+        <br></br>
+        <b>Mauris id nibh eu fermentum mattis purus?</b>
+        <br></br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nisi ipsa dicta. Ullam minus ab officiis ex placeat, iusto quibusdam facere odio id architecto quia cupiditate nisi velit magni voluptatem!
+        <br></br>
+        <b>Eros imperdiet rhoncus?</b>
+        <br></br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptatibus numquam fugit deserunt odit quam totam obcaecati dolor atque? Commodi, et unde. Fugiat dignissimos, provident recusandae culpa exercitationem ipsa ea!
+        <br></br>
+        <b>Fames imperdiet quam fermentum?</b>
+         <br></br>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et blanditiis est, error qui corporis voluptate, quia similique accusantium quo ex voluptas nostrum voluptates modi! Officiis exercitationem aliquid hic culpa natus?
+        <br></br>
+        <b>Varius vitae, convallis amet lacus sit aliquet nibh?</b>
+        <br></br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolor provident cupiditate sunt cum, ullam quis quae pariatur nihil inventore quo odit, adipisci impedit hic, quas quaerat laboriosam aliquid illo!
+        <br></br>
+        <b>Tortor nisi pellentesque sit quis orci dolor</b>
+        <br></br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aut odit ratione iusto cupiditate rem sunt provident necessitatibus, consequatur quia inventore quod cumque suscipit incidunt in, recusandae itaque vel! Corporis!
+      </div>
+    </div>
   )
 }
 
--- src/Components/Section/OurStory/OurStory.jsx
+++ src/Components/Section/OurStory/OurStory.jsx
@@ -3,7 +3,42 @@ import styles from './OurStory.module.css'
 
 const OurStory = () => {
   return (
-    <div className={styles.ourStory}>OurStory</div>
+    <div className={styles.ourStory}>
+      <span className='Intro'>
+        <b>OurStory</b>
+      </span>
+
+      <br></br>
+      
+      <span className='text'>
+        <p>We know farming is hard work. Whether you grow crops, raise livestock, or catch fish,
+          you deserve fair prices for your efforts.
+        </p>
+      </span>
+
+      <br></br>
+
+      <span className='text'>
+        <p>Many farmers work alone. Middlemen take profits. Language barriers
+          block opportunities. Good farmers struggle while others prosper.
+        </p>
+      </span>
+
+      <br></br>
+
+      <span className='text'>
+        <p>That's why we built BubeTech. We connect every farmer - from small gardens to large 
+         herds - to the knowledge, quality resources, and fair prices they need to succeed.
+          Your phone becomes your farming partner. Your voice matters. Your community grows
+          stronger.
+        </p>
+      </span>
+
+      <br></br>
+      
+      <span className='bottom'><b>Farm smarter, live better. When farmers connect, everyone wins.</b></span>    
+    
+    </div>
   )
 }
 
