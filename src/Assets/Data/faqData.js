c1909dc144b7822447ab0f76e228174ed0c6e53e
Oluwadamilola S. Odujirin <oluwadamilolaod@ha-shem.com>
2025-07-01 19:07:22 +0100

    update

---
+++ src/Assets/Data/faqData.js
@@ -0,0 +1,32 @@
+export const faqData = [
+  {
+    question: "Where can I watch?",
+    answer:
+      "Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis.",
+  },
+  {
+    question: "Mauris id nibh eu fermentum mattis purus?",
+    answer:
+      "Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis.",
+  },
+  {
+    question: "Eros imperdiet rhoncus?",
+    answer:
+      "Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis.",
+  },
+  {
+    question: "Fames imperdiet quam fermentum?",
+    answer:
+      "Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis.",
+  },
+  {
+    question: "Varius vitae, convallis amet lacus sit aliquet nibh?",
+    answer:
+      "Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis.",
+  },
+  {
+    question: "Tortor nisl pellentesque sit quis orci dolor?",
+    answer:
+      "Nibh quisque suscipit fermentum netus nulla cras porttitor euismod nulla. Orci, dictumst nec aliquet id ullamcorper venenatis.",
+  },
+];
